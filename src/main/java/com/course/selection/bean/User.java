71b45ddf7bcfae7f8b6f2d717d99395f739b7a14
weiij <wei.liang@warp-tech.cn>
2018-12-18 21:01:27 +0800

    20181218

---
+++ src/main/java/com/course/selection/bean/User.java
@@ -0,0 +1,43 @@
+package com.course.selection.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * 用户信息
+ * @author lw
+ * @data 2018/10/25
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer uid;
+    private String openId;//微信接口返回的唯一id
+    private String unionId;//
+    private String nickname;//用户昵称
+    private String avatar;//用户头像url
+    private Integer gender;//用户性别
+    private LocalDateTime createTime;//创建时间
+    private LocalDateTime activeTime;//活跃时间
+    private String inviteCode;//邀请码
+    @Builder.Default
+    private String channel="default";//用户渠道
+    private String ip;//第一次登陆的ip地址
+    private String city;//城市
+    private String province;//省份
+    private String country;//国家
+    private String version;//用户注册版本
+    private String shareImg;//用户是通过哪个分享图进来的
+    private Integer shareId;//被哪个用户邀请进来的
+    private String randomId;//签到标记
+    private Integer signInDays;//签到天数
+
+
+}
---
+++ src/main/java/com/course/selection/config/App.java
@@ -0,0 +1,13 @@
+package com.course.selection.config;
+
+public class App {
+//    f0fbde0f782d1c5223c397efaa3b4a30
+    public static final String APP_ID = "wxa3ebad20e25a3493";
+    public static final String APP_SECRET = "f0fbde0f782d1c5223c397efaa3b4a30";
+    public static final String LOGIN_API = "https://api.weixin.qq.com/sns/jscode2session";
+    public static final String CUSTOMER_TOKEN = "shan1";
+    public static final String SEND_MSG = "https://api.weixin.qq.com/cgi-bin/message/custom/send";
+    public static final String TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token";
+    public static final String SEND_MEDIA = "https://api.weixin.qq.com/cgi-bin/media/upload";
+
+}
